20d29808219dc89eb17f9fdf0a7245b2ef5aea42
wanghazing <15216124348@163.com>
2023-10-19 09:43:47 +0800

    更新env

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, ConfigEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import { resolve } from "path";
@@ -10,27 +10,30 @@ const pathResolve = (dir: string) => {
 const alias: Record<string, string> = {
   "/@": pathResolve("./src/"),
 };
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: { alias },
-  base: "/ugi/",
-  server: {
-    host: "0.0.0.0",
-    port: 8000,
-    open: false,
-    hmr: true,
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:1444/",
-        ws: true,
-        changeOrigin: true,
-        // rewrite: (path) => {
-        //   console.log(path)
-        //   return path.replace(/^\/api/, "")
-        // },
+export default defineConfig((mode: ConfigEnv) => {
+  const env = loadEnv(mode.mode, process.cwd());
+  console.log(env);
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: { alias },
+    base: env.VITE_BASE_DIR,
+    server: {
+      host: "0.0.0.0",
+      port: 8000,
+      open: false,
+      hmr: true,
+      proxy: {
+        "/api": {
+          target: "http://127.0.0.1:1444/",
+          ws: true,
+          changeOrigin: true,
+          // rewrite: (path) => {
+          //   console.log(path)
+          //   return path.replace(/^\/api/, "")
+          // },
+        },
       },
     },
-  },
+  };
 });
